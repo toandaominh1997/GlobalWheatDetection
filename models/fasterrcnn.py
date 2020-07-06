4c012c03a0bd63d6ca880456f7927c99bc742011
toandm2 <toandm2@onemount.com>
2020-07-06 11:14:26 +0000

    Update dataset training and testing

---
+++ models/fasterrcnn.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.models.detection.backbone_utils import BackboneWithFPN
+from torchvision.ops import misc as misc_nn_ops
+from timm.models.resnest import resnest101e
+
+
+class CrossEntropyLabelSmooth(nn.Module):
+    """Cross entropy loss with label smoothing regularizer.
+
+    Reference:
+    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
+    Equation: y = (1 - epsilon) * y + epsilon / K.
+
+    Args:
+        num_classes (int): number of classes.
+        epsilon (float): weight.
+    """
+    def __init__(self, num_classes, epsilon=0.1, use_gpu=True):
+        super(CrossEntropyLabelSmooth, self).__init__()
+        self.num_classes = num_classes
+        self.epsilon = epsilon
+        self.use_gpu = use_gpu
+        self.logsoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, inputs, targets):
+        """
+        Args:
+            inputs: prediction matrix (before softmax) with shape (batch_size, num_classes)
+            targets: ground truth labels with shape (num_classes)
+        """
+        log_probs = self.logsoftmax(inputs)
+        targets = torch.zeros(log_probs.size()).scatter_(1, targets.unsqueeze(1).data.cpu(), 1)
+        if self.use_gpu: targets = targets.cuda()
+        targets = (1 - self.epsilon) * targets + self.epsilon / self.num_classes
+        loss = (- targets * log_probs).mean(0).sum()
+        return loss
+
+def fastrcnn_loss(class_logits, box_regression, labels, regression_targets):
+    # type: (Tensor, Tensor, List[Tensor], List[Tensor]) -> Tuple[Tensor, Tensor]
+    """
+    Computes the loss for Faster R-CNN.
+    Arguments:
+        class_logits (Tensor)
+        box_regression (Tensor)
+        labels (list[BoxList])
+        regression_targets (Tensor)
+    Returns:
+        classification_loss (Tensor)
+        box_loss (Tensor)
+    """
+
+    labels = torch.cat(labels, dim=0)
+    regression_targets = torch.cat(regression_targets, dim=0)
+    labal_smooth_loss = CrossEntropyLabelSmooth(2)
+    classification_loss = labal_smooth_loss(class_logits, labels)
+
+    # get indices that correspond to the regression targets for
+    # the corresponding ground truth labels, to be used with
+    # advanced indexing
+    sampled_pos_inds_subset = torch.nonzero(labels > 0).squeeze(1)
+    labels_pos = labels[sampled_pos_inds_subset]
+    N, num_classes = class_logits.shape
+    box_regression = box_regression.reshape(N, -1, 4)
+
+    box_loss = det_utils.smooth_l1_loss(
+        box_regression[sampled_pos_inds_subset, labels_pos],
+        regression_targets[sampled_pos_inds_subset],
+        beta=1 / 9,
+        size_average=False,
+    )
+    box_loss = box_loss / labels.numel()
+
+    return classification_loss, box_loss
+
+def resnest_fpn_backbone(pretrained, norm_layer=misc_nn_ops.FrozenBatchNorm2d, trainable_layers=3):
+    # resnet_backbone = resnet.__dict__['resnet152'](pretrained=pretrained,norm_layer=norm_layer)
+    backbone = resnest101e(pretrained=pretrained)
+    # select layers that wont be frozen
+    assert trainable_layers <= 5 and trainable_layers >= 0
+    layers_to_train = ['layer4', 'layer3', 'layer2', 'layer1', 'conv1'][:trainable_layers]
+    # freeze layers only if pretrained backbone is used
+    for name, parameter in backbone.named_parameters():
+        if all([not name.startswith(layer) for layer in layers_to_train]):
+            parameter.requires_grad_(False)
+    return_layers = {'layer1': '0', 'layer2': '1', 'layer3': '2', 'layer4': '3'}
+    in_channels_stage2 = backbone.inplanes // 8
+    in_channels_list = [
+        in_channels_stage2,
+        in_channels_stage2 * 2,
+        in_channels_stage2 * 4,
+        in_channels_stage2 * 8,
+    ]
+    out_channels = 256
+    return BackboneWithFPN(backbone, return_layers, in_channels_list, out_channels)
+
+
+class WheatDetector(torch.nn.Module):
+    def __init__(self, trainable_layers=3, **kwargs):
+        super(WheatDetector, self).__init__()
+        backbone = resnest_fpn_backbone(pretrained=False)
+        self.base = FasterRCNN(backbone, num_classes = 2, **kwargs)
+        self.base.roi_heads.fastrcnn_loss = fastrcnn_loss
+
+    def forward(self, images, targets=None):
+        return self.base(images, targets)
+
--- train.py
+++ train.py
@@ -12,6 +12,11 @@ from datasets.dataset import WheetDataset
 import torchvision
 from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator
+from models import WheatDetector
+
+
+
+
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('--network', default='fasterrcnn_resnet50_fpn', type=str,
                                     help='efficientdet-[d0, d1, ..]')
@@ -89,17 +94,18 @@ if(args.resume is not None):
     args.start_epoch = checkpoint['epoch'] + 1
     del params
 # model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False, progress=True, num_classes=2, pretrained_backbone=True)
-backbone = torchvision.models.mobilenet_v2(pretrained=True).features
-backbone.out_channels = 1280
-anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
-                                           aspect_ratios=((0.5, 1.0, 2.0),))
-roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
-                                                        output_size=7,
-                                                         sampling_ratio=2)
-model = FasterRCNN(backbone,
-                    num_classes=2,
-                    rpn_anchor_generator=anchor_generator,
-                     box_roi_pool=roi_pooler)
+# backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+# backbone.out_channels = 1280
+# anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+#                                            aspect_ratios=((0.5, 1.0, 2.0),))
+# roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
+#                                                         output_size=7,
+#                                                          sampling_ratio=2)
+# model = FasterRCNN(backbone,
+#                     num_classes=2,
+#                     rpn_anchor_generator=anchor_generator,
+#                      box_roi_pool=roi_pooler)
+model = WheatDetector()
 if(args.resume is not None):
     model.load_state_dict(checkpoint['state_dict'])
 del checkpoint
